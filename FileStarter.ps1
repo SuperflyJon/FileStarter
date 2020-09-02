71f32e72344cb31f39831a375c8b4cb7dd444d6a
SuperflyJon <JonKinsey@gmail.com>
2020-09-02 16:26:49 +0100

    Sign scripts and fix not closing when starting Visual Code

--- FileStarter.ps1
+++ FileStarter.ps1
@@ -350,11 +350,11 @@ function StartProcess($object)
 
         if ($object.arguments.count -gt 0)
         {
-            $object.proc = Start-Process $process -ArgumentList $object.arguments -PassThru;
+            $object.proc = Start-Process $process -ArgumentList $object.arguments -PassThru
         }
         else
         {
-            $object.proc = Start-Process $process -PassThru;
+            $object.proc = Start-Process $process -PassThru
         }
 
         if ($object.waitForClass)
@@ -617,5 +617,44 @@ $currentWindow | UnPin-Window
 $currentWindow | Move-Window $currentDesktop | Out-Null
 $currentDesktop | Switch-Desktop
 [Win.API]::SetWindowNotOnTop($currentWindow)
+ 
+$childConsole = Get-WmiObject win32_process | where {($_.ParentProcessID -eq $pid) -and ($_.Name -eq "conhost.exe")}
+if ($childConsole)
+{
+    Stop-Process $childConsole.ProcessID
+}
 
 write-host "fin"
+
+# SIG # Begin signature block
+# MIIFdgYJKoZIhvcNAQcCoIIFZzCCBWMCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUUc9kY1eFmBFafiimIF6Jm2ir
+# PhqgggMOMIIDCjCCAfKgAwIBAgIQJ7QhgRtobKJFyrX3zvZHpjANBgkqhkiG9w0B
+# AQUFADAdMRswGQYDVQQDDBJMb2NhbCBDb2RlIFNpZ25pbmcwHhcNMjAwOTAyMTUw
+# NjQ0WhcNMjEwOTAyMTUyNjQ0WjAdMRswGQYDVQQDDBJMb2NhbCBDb2RlIFNpZ25p
+# bmcwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDZgckf5EM3ekbD9UHM
+# uPAb3n+ruyIkD5y/uRfKbjQ9R6hpjzz5MK8qD9EOPn1rX9QUy1QJyuGQENCbOAot
+# k9UE6NUtdv6cOT+4EHtcyfpW03l1LWaBpplS+4CBddfig03rpyaqQuVJe93WQ4Ar
+# eUAvWozM9/Smt39MxwsyneTAHWzPFJnBXBry3OfGqg7sfJdFh02+KDOfgBfDUrGT
+# dFiXzwPyZt1MfuHbQUvd+o1ErneWSqKXFjnxP/8va7mSLFjXEDZqLhm+p96jlGEs
+# 0nNSLf7AfG/ZMTKfMY/X77fd08U1Dy//xc/e4qkraQfiN5TJCz8im/E6WFzrF1eQ
+# XRJFAgMBAAGjRjBEMA4GA1UdDwEB/wQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcD
+# AzAdBgNVHQ4EFgQU9nmxQrbevGH/Lmr0GxeZa9LIwJ0wDQYJKoZIhvcNAQEFBQAD
+# ggEBAC4XGOmt8kosp5Wdu5FbVmLUanHNUcA0S46UyEyNK8yBDnGiNJf0hVG5D/Jh
+# w4mk3BuKZjFcsV5bA1ZKcmE6hrUFxmGGfgnFmEGmdI+Tt9vR0bv3re7SG4jlaMnu
+# VgOEjn+AIpon8djn10Warb6q45Q28O+3lWP0D832Sx69sI06QNbpzB88QQZJNgBD
+# 29AmDvESdzCyCRH1aJAN93AgjwmXkTzgfnWL3N6myKYRMmSfShdJD2s/GxoB0hZy
+# 1OleXmsYOEZP9KUkqo5p7eW0Koxq0EdzTmuvJSUeRr7MT+PpVrp/vaKYRg/DTGxW
+# jcTmxfUgz2AjWi+8UcjXZAjw95kxggHSMIIBzgIBATAxMB0xGzAZBgNVBAMMEkxv
+# Y2FsIENvZGUgU2lnbmluZwIQJ7QhgRtobKJFyrX3zvZHpjAJBgUrDgMCGgUAoHgw
+# GAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGC
+# NwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQx
+# FgQUEVvgsPwQL/2pkQ0KlVTZpVggSI8wDQYJKoZIhvcNAQEBBQAEggEAzH8fGKg3
+# O3GIaU5qIMsDaqlU31KMo8zYPBgz5Auz2V15htbYhNUXY1S5zGmAhLr9aTZvUrjG
+# hNYwLf8+AzxLYHj/HyQROrNHmwgfugcVT/WmBuzo8d98JShrijui0C6nOPTu9Nzi
+# hFkm7Q91OZtUnsjjRMYgDoh5n0MldrUAXqz9CF9GSsPhdwa0jXPWBgG3pBvwT2cN
+# S4lFnYWEXcMK2wEjky+5akqw/npvlZ8sYyUUXRQMvWx28RJvy4xbFZe81eCSyGMQ
+# 6tqe7ee5wLXlsucXbEKrDtTlK679lUmb8gi92k6UGvw8vPRN8DfiDP1Bd/pCO3sQ
+# UxOm8v0ZD3dOXg==
+# SIG # End signature block
